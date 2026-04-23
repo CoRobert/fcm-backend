ebff7f4299986e9a92292052ea69ab268e8331ba
CoRobert <roberto_gonzalezvera@hotmail.com>
2026-04-23 01:33:08 -0600

    fix port for railway

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/send-notification', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🚀 Server corriendo en http://localhost:${PORT}`);
 });
